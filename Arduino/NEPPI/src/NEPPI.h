a24a1f6403207825a88206d972a2ed8aa20bc096
AleksiZub <90851861+AleksiZub@users.noreply.github.com>
2021-10-06 13:46:37 +0300

    Update NEPPI.h

--- Arduino/NEPPI/src/NEPPI.h
+++ Arduino/NEPPI/src/NEPPI.h
@@ -12,7 +12,9 @@ class NEPPI
    public:
    NEPPI();
     void step(Stepper _stepper, int nrsteps, int speed, int delnum = 0);
-    void servo(Servo _servo, int posinit, int posend, int delnum);
+   
+    // Servo doesn't work due to "timer stuff"
+    //void servo(Servo _servo, int posinit, int posend, int delnum);
   private:
   
 
